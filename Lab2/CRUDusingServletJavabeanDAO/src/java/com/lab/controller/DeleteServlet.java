5cc1f8e8c2b0136cdcfa0c14776b04e44e262fe0
s76216-rgb <s76216@ocean.umt.edu.my>
2026-04-08 21:54:26 +0800

    Initial push of lab report

---
+++ Lab2/CRUDusingServletJavabeanDAO/src/java/com/lab/controller/DeleteServlet.java
@@ -0,0 +1,30 @@
+package com.lab.controller;
+
+import com.lab.dao.UserDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "DeleteServlet", urlPatterns = {"/DeleteServlet"})
+public class DeleteServlet extends HttpServlet {
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDAO.deleteUser(id); // Panggil DAO
+
+        response.sendRedirect("ViewServlet");
+    }
+}
+
+
+
+
